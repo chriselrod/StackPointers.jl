23e434fa95d49eb46e6b28bb4ef80cafc7ad4411
Chris Elrod <elrodc@gmail.com>
2019-12-19 03:20:36 -0500

    Updated compat and precompile.

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,4 +1,5 @@
 function _precompile_()
     ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
     isdefined(StackPointers, Symbol("#1#2")) && precompile(Tuple{getfield(StackPointers, Symbol("#1#2")),Expr})
+    precompile(Tuple{typeof(Base.setindex_widen_up_to),Array{Symbol,1},StackPointers.StackPointer,Int64})
 end
